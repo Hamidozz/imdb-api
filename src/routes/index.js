ccbed548fec2e3c5aabb806601e3579cf459f12e
Hamidozz <es-ahmed.salem1116@alexu.edu.eg>
2023-09-03 21:11:00 +0300

    Update index.js

--- src/routes/index.js
+++ src/routes/index.js
@@ -11,10 +11,6 @@ index.get("/", async (c) => {
     repository: packageJson.homepage,
     author: packageJson.author,
     license: packageJson.license,
-    postman:
-      "https://www.postman.com/tuhin-pal/workspace/imdb-api/collection/12162111-12f08f8e-a76b-4cf4-a7b9-17cb9f95dd82?action=share&creator=12162111",
-    postman_collection_json:
-      "https://www.getpostman.com/collections/c261b9abc6b2a4b5f1c8",
   });
 });
 
